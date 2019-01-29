67beaf9e82306d37b9a5892a91089bd40901034a
Nico <mouet.nicolas@gmail.com>
2019-01-29 12:19:45 +0100

    début

--- histoires.py
+++ histoires.py
@@ -1,1 +1,8 @@
 #Contiendra les fonctions pour choisir et charger une histoire
+liste_histoires=["a","b","c"]
+print(liste_histoires[0])
+print(liste_histoires[1])
+print(liste_histoires[2])
+titre=input("choisir le titre de votre histoire")
+
+	pass
