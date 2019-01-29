13c709eb4d91634aa4fa3c17f5ec342400db9c12
Arthur <arthur.wacquez@gmail.com>
2019-01-29 11:54:07 +0100

    ajout return

--- classLieu.py
+++ classLieu.py
@@ -1,10 +1,13 @@
 #Définition de la classe lieu : attributs et méthodes
 
-class lieu:
+class Lieu:
     def __init__(self, identifiant, nom, description, adjacence):
       self.identifiant = identifiant
       self.nom = nom
       self.description = description
       self.adjacence = adjacence
-    def__repr__(self):
-      pass
+    def __repr__(self):
+         s=self.nom
+         s+= " ~~ \n"
+         s+=self.description
+         return(s)
