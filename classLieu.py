f9d80f38197f4010ad1e35a0ada202c2bd1fd2be
Arthur <arthur.wacquez@gmail.com>
2019-01-29 11:34:05 +0100

    repr

--- classLieu.py
+++ classLieu.py
@@ -1,8 +1,8 @@
 #Définition de la classe lieu : attributs et méthodes
 
 class lieu:
-  def __init__(self, identifiant, nom, description, adjacence):
-    self.identifiant = identifiant
-    self.nom = nom
-    self.description = description
-    self.adjacence = adjacence
+    def __init__(self, identifiant, nom, description, adjacence):
+      self.identifiant = identifiant
+      self.nom = nom
+      self.description = description
+      self.adjacence = adjacence
