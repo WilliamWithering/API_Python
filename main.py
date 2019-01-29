fef5bf4b734e9f6d073bbadeeedaacb7d00654a9
William Withering <simlep4@gmail.com>
2019-01-29 11:52:17 +0100

    Création des contrôles du jeu

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
 #FICHIER PRINCIPAL : EXECUTION DU JEU
+
+import classJeu
+import classLieu
+import classPersonnage
+import histoires
+
+#Initialisation des variables utilisées dans main
+titreChoisi = ""
+transition = 1
+
+histoires.afficherTitres()
+
+while not histoires.titre_correct(titreChoisi):
+    if titreChoisi :
+        print("Titre choisi incorrect")
+    titreChoisi = input("Choisir un titre d'histoire : ")
+
+jeu = histoires.charger_jeu(titreChoisi)
+
+jeu.afficher_regles()
+
+jeu.set_nom_personnage(input())
+
+#On commence vraiment le jeu
+while not jeu.est_fini():
+    if transition:
+        jeu.decrire()
+        transition = 0
+    commande = input()
+    jeu.execute(commande)
