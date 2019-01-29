575d38fa876c7f73494b722a6f13bce68cb27d17
Nico <mouet.nicolas@gmail.com>
2019-01-29 11:59:17 +0100

    liste lieu

--- jeu.py
+++ jeu.py
@@ -1,13 +1,13 @@
-import classLieu
-import classPersonnage
-
-class Jeu:
-	def __init__(self):
-		self.lieu_initial="0"
-		lieu=()
-
-
-
-
-lieu_initial=class_lieu()
+import classLieu 
+import classPersonnage 
+ 
+class Jeu: 
+	def __init__(self): 
+		self.lieu_initial="0" 
+		self.identifiant = identifiant 
+    	self.description = description 
+    	self.adjacence = adjacence 
+     	lieu=[]
+ 
+lieu_initial=class_lieu() 
 lieu_initial.nom
