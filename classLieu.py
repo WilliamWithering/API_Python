69b2c1b6f5c9ca7a55b0afc4fe7f92a033ec02d0
Arthur <arthur.wacquez@gmail.com>
2019-01-29 11:29:54 +0100

    Ajout init

--- classLieu.py
+++ classLieu.py
@@ -1,4 +1,8 @@
 #Définition de la classe lieu : attributs et méthodes
 
-class Lieu:
-    pass
+class lieu:
+  def __init__(self, identifiant, nom, description, adjacence):
+    self.identifiant = identifiant
+    self.nom = nom
+    self.description = description
+    self.adjacence = adjacence
