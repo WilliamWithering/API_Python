e9a43c4c65f63c26d9633cc7fb087555383f6ef8
Nico <mouet.nicolas@gmail.com>
2019-01-29 12:29:32 +0100

    création de while

--- histoires.py
+++ histoires.py
@@ -1,8 +1,15 @@
-#Contiendra les fonctions pour choisir et charger une histoire
-liste_histoires=["a","b","c"]
-print(liste_histoires[0])
-print(liste_histoires[1])
-print(liste_histoires[2])
-titre=input("choisir le titre de votre histoire")
+def choisir_titre():
+	liste_histoires=["a","b","c"]
+	print("Voici la liste des titres possibles pour créer votre propre histoire") 
+	print(liste_histoires[0]) 
+	print(liste_histoires[1]) 
+	print(liste_histoires[2]) 
+	
+	titre_choisi=""
 
-	pass
+	while not titre_choisi in liste_histoires:
+    	if titre_choisi :
+        	print("Titre choisi incorrect.")
+    	titre_choisi = input("Choisir un titre d'histoire : ")
+
+	return titre_choisi
